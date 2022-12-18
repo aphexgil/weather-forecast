c14e2e615f9f5f74ec15c34be01f993bb1270429
aphexgil <gil.young.1994@gmail.com>
2022-12-18 13:22:19 -0800

    Added little x's to each saved search button which remove associated button if clicked. In the process of adapting code from https://htmldom.dev/drag-and-drop-element-in-a-list/ to make save search buttons drag-and-drop reorderable in the list so a user can put their most relevant searches at the top.

--- Assets/script.js
+++ Assets/script.js
@@ -2,47 +2,39 @@ const key = '37dc5cb97cecc77ea9e04e3af68fc80b';
 let weatherInfo;
 let pastSearches = JSON.parse(localStorage.getItem("pastSearches"));
 
-function saveSearch(city){
-    if(pastSearches==null){
-        pastSearches = [];
-    }
-    let beenSearched = false;
-    for(var i=0; i<pastSearches.length; i++){
-        if(pastSearches[i].toUpperCase() == city.toUpperCase()){
-            beenSearched = true;
-        }
-    }
-    if(!beenSearched){
-        pastSearches.push(city);
-        localStorage.setItem("pastSearches",JSON.stringify(pastSearches));
-    }
-    displaySearches();
-}
-
 async function getWeather(city){
 
     let geoCodingURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + "&appid=" + key;
-    let cityInfo;
+    let cityInfo = null;
     await fetch(geoCodingURL)
         .then((resp) => resp.json())
         .then( function(data) {
             cityInfo = data;
         })
-        .catch( (error) => {
-            console.log('error');
+        .catch( function(error) {
+            console.log(error);
+            window.alert('City not found');
         });
-    let weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityInfo[0].lat + '&lon=' + cityInfo[0].lon + "&appid=" + key;
-    await fetch(weatherURL)
-        .then((resp) => resp.json())
-        .then( function(data) {
-            weatherInfo = data;
-            displayWeather();
-        })
+    if(cityInfo.length != 0){
+        let weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityInfo[0].lat + '&lon=' + cityInfo[0].lon + "&appid=" + key;
+        await fetch(weatherURL)
+            .then((resp) => resp.json())
+            .then( function(data) {
+                weatherInfo = data;
+                saveSearch(city);
+                displayWeather();
+            })
+            .catch( (error) => {
+                console.log(error);
+                window.alert('City not found');
+            });
+    }
+    
 }
 
 function displayWeather(){
 
-    $('.spacer').remove();
+    $('.spacer').attr('style', 'display:none');
     $('.big-col').remove();
 
     var bigCol = $('<div/>',{
@@ -166,7 +158,30 @@ function displayWeather(){
     $('.main-row').append(bigCol);
 }
 
+function saveSearch(city){
+    if(pastSearches==null){
+        pastSearches = [];
+    }
+    let beenSearched = false;
+    for(var i=0; i<pastSearches.length; i++){
+        if(pastSearches[i] == weatherInfo.city.name){
+            beenSearched = true;
+            break;
+        }
+    }
+    if(!beenSearched){
+        pastSearches.push(city);
+        localStorage.setItem("pastSearches",JSON.stringify(pastSearches));
+    }
+    displaySearches();
+}
 
+function removeSearch(x){
+    let cityName = x.parent().text().slice(0,-1);
+    pastSearches = pastSearches.filter( e => e !== cityName);
+    localStorage.setItem("pastSearches",JSON.stringify(pastSearches));  
+    displaySearches();
+}
 
 function displaySearches(){
     $('.saved-searches').empty();
@@ -176,15 +191,22 @@ function displaySearches(){
     }
 ;   for(var i=0; i<pastSearches.length; i++){
         let cityName = pastSearches[i];
-        let btn = $('<button/>',
-        {
-            text: cityName,
-            class: 'search-again'
-        });
+        let btn = $('<div>');
+        btn.text(cityName);
+        btn.attr('class', 'search-again draggable');
+        btn.attr('style', 'text-align: center; cursor: pointer;');
         $(btn).on('click', function () {
             getWeather(cityName);
         });
-        $('.saved-searches').append(btn, '<br>');
+        $('.saved-searches').append(btn);
+        let x = $('<span/>');
+        x.text('x');
+        x.attr('style','float: right; margin-right: 11px; cursor: pointer; font-weight: bolder;');
+        btn.append(x);
+        $(x).on('mousedown', function (event) {
+            event.stopPropogation;
+            removeSearch(x);
+        });
     }
     var clearBtn = $('<button/>',
     {
@@ -205,7 +227,6 @@ function handleFormSubmission(){
     let city = $('.city-input').val();
     $('.city-input').val('');
     if(city !== '' && city !== null){
-        saveSearch(city);
         getWeather(city);
     }
 }
@@ -219,3 +240,137 @@ $('.input-form').on("submit", function (event){
     event.preventDefault();
     handleFormSubmission();
 });
+
+
+
+
+//------ FOLLOWING CODE COPIED/ADAPTED FROM https://htmldom.dev/drag-and-drop-element-in-a-list/ 
+
+
+
+
+// The current dragging item
+let draggingEle;
+
+// The current position of mouse relative to the dragging element
+let x = 0;
+let y = 0;
+
+const mouseDownHandler = function (e) {
+    draggingEle = e.target;
+
+    let dragCity = draggingEle.textContent.slice(0,-1);
+    console.log(dragCity);
+    // Calculate the mouse position
+    const rect = draggingEle.getBoundingClientRect();
+    x = e.pageX - rect.left;
+    y = e.pageY - rect.top;
+
+    // Attach the listeners to `document`
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
+// Query the list element
+const list = document.getElementById('list');
+
+// Query all items
+Array.prototype.slice.call(list.querySelectorAll('.draggable')).forEach(function (item) {
+    item.addEventListener('mousedown', mouseDownHandler);
+});
+
+let placeholder;
+let isDraggingStarted = false;
+
+const mouseMoveHandler = function(e) {
+
+    let w = draggingEle.offsetWidth;
+    let h = draggingEle.offsetHeight;
+    // Set position for dragging element
+    draggingEle.style.position = 'absolute';
+    draggingEle.style.top = `${e.pageY - y}px`;
+    draggingEle.style.left = `${e.pageX - x}px`;
+    draggingEle.style.width = `${w}px`;
+    const draggingRect = draggingEle.getBoundingClientRect();
+
+    if (!isDraggingStarted) {
+        // Update the flag
+        isDraggingStarted = true;
+
+        // Let the placeholder take the height of dragging element
+        // So the next element won't move up
+        placeholder = document.createElement('div');
+        placeholder.classList.add('placeholder');
+        draggingEle.parentNode.insertBefore(
+            placeholder,
+            draggingEle.nextSibling
+        );
+
+        // Set the placeholder's height
+        placeholder.style.height = `${4/3*h}px`;
+    }
+    const prevEle = draggingEle.previousElementSibling;
+    const nextEle = placeholder.nextElementSibling;
+
+    // User moves item to the top
+    if (prevEle && isAbove(draggingEle, prevEle)) {
+        // The current order    -> The new order
+        // prevEle              -> placeholder
+        // draggingEle          -> draggingEle
+        // placeholder          -> prevEle
+        swap(placeholder, draggingEle);
+        swap(placeholder, prevEle);
+        return;
+    }
+
+    // User moves the dragging element to the bottom
+    if (nextEle && isAbove(nextEle, draggingEle)) {
+        // The current order    -> The new order
+        // draggingEle          -> nextEle
+        // placeholder          -> placeholder
+        // nextEle              -> draggingEle
+        swap(nextEle, placeholder);
+        swap(nextEle, draggingEle);
+    }
+}
+
+const mouseUpHandler = function() {
+    
+    console.log(draggingEle.parentNode);
+
+    // Remove the position styles
+    draggingEle.style.removeProperty('top');
+    draggingEle.style.removeProperty('left');
+    draggingEle.style.removeProperty('position');
+
+    x = null;
+    y = null;
+    draggingEle = null;
+
+    // Remove the handlers of `mousemove` and `mouseup`
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+    // Remove the placeholder
+    placeholder && placeholder.parentNode.removeChild(placeholder);
+    // Reset the flag
+    isDraggingStarted = false;
+};
+
+const isAbove = function (nodeA, nodeB) {
+    // Get the bounding rectangle of nodes
+    const rectA = nodeA.getBoundingClientRect();
+    const rectB = nodeB.getBoundingClientRect();
+
+    return rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2;
+};
+
+const swap = function (nodeA, nodeB) {
+    const parentA = nodeA.parentNode;
+    const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+    // Move `nodeA` to before the `nodeB`
+    nodeB.parentNode.insertBefore(nodeA, nodeB);
+
+    // Move `nodeB` to before the sibling of `nodeA`
+    parentA.insertBefore(nodeB, siblingA);
+};
